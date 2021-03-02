f38f8c1fc10549f1e96acf8654a3a3e057edc27e
Isaac King <idrkings@gmail.com>
2021-03-02 04:48:29 +0000

    Completed exercise 8

--- lib/store.rb
+++ lib/store.rb
@@ -4,9 +4,19 @@ class Store < ActiveRecord::Base
   validate :has_men_or_womens_apparel
   has_many :employees
 
+  before_destroy :check_employees
+
   def has_men_or_womens_apparel 
     unless mens_apparel || womens_apparel
       errors.add(:mens_apparel, "Must carry either men or women's apparel")
+    end
+  end
+
+  private
+
+  def check_employees
+    unless self.employees.size == 0
+      return false
     end
   end
 end
